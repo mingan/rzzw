4dfb45c52a3ecd81d3b4973d21e2b77e8eaf8663
Štěpán Pilař <ja@mingan.name>
2013-01-04 18:49:16 +0100

    Namespaced JS

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-var dbpSparql = 'http://dbpedia.org/sparql';
+var rzzw = {};
+var map = {};
 
-jQuery(document).ready(function ($) {
-    var $OGSelector = $('#OGSelector');
-    var $Olympics = $('#Olympics');
-
-    var $EventsSelector = $('#EventsSelector');
-    var $Events = $('#Events');
-
-    var $DisciplinesSelector = $('#DisciplinesSelector');
-    var $Disciplines = $('#Disciplines');
-
-    var $Winners = $('#Winners');
-    var $Map = $('#Map');//.hide();
+(function () {
+    var $Map = $('#Map');
     var map = new google.maps.Map(
         document.getElementById("Map"),
         {
@@ -23,134 +14,12 @@ jQuery(document).ready(function ($) {
     );
     var geocoder = new google.maps.Geocoder();
 
-
-    function repmoveStringFromElements (str, $els) {
-        $els.each(function (i, e) {
-            $(e).text($(e).text().replace(str, ''));
-        });
-    }
-
-    function loadOlympics () {
-        $Olympics.load('dispatch.php?source=freebase&type=games', null, function () {
-            olympicsLoaded();
-        });
-    }
-    function olympicsLoaded () {
-        $OGSelector
-            .removeClass('loading')
-            .find('span')
-                .text('Choose Olympic Games')
-            .end()
-            .click();
-
-        $Olympics.find('a').click(function (e) {
-            e.preventDefault();
-
-            $OGSelector.find('span').text($(this).text()).attr('data-og_name', $(this).attr('data-og_name'));
-            var mid = $(this).attr('href').replace('#', '');;
-
-            loadEventsFor(mid);
-        });
-    }
-
-    function loadEventsFor (mid) {
-        $EventsSelector
-            .parent('.hide')
-            .removeClass('hide')
-            .end()
-            .addClass('loading')
-            .find('span').text('Loading...');
-
-        $Events.load('dispatch.php?source=freebase&type=events&params=' + mid, null, function () {
-            var currentName = new RegExp(' at (the)? ' + $OGSelector.find('span').attr('data-og_name') + '?');
-            repmoveStringFromElements(currentName, $Events.find('li a'));
-            eventsLoaded();
-        });
-    }
-    function eventsLoaded () {
-        $EventsSelector
-            .removeClass('loading')
-            .find('span')
-                .text('Choose an event')
-            .end()
-            .click();
-
-        $Events.find('a').click(function (e) {
-            e.preventDefault();
-
-            $EventsSelector.find('span').text($(this).text());
-            var mid = $(this).attr('href').replace('#', '');
-
-            loadDisciplinesFor(mid);
-        });
-    }
-
-    function loadDisciplinesFor (mid) {
-        $DisciplinesSelector
-            .parent('.hide')
-            .removeClass('hide')
-            .end()
-            .addClass('loading')
-            .find('span').text('Loading...');
-
-        $Disciplines.load('dispatch.php?source=freebase&type=disciplines&params=' + mid, null, function () {
-            var currentName = new RegExp($EventsSelector.find('span').text() + ' at (the)? ' + $OGSelector.find('span').attr('data-og_name') + '?' + ' – ');
-            repmoveStringFromElements(currentName, $Disciplines.find('li a'));
-            disciplinesLoaded();
-        });
-    }
-
-    function disciplinesLoaded () {
-        $DisciplinesSelector
-            .removeClass('loading')
-            .find('span')
-                .text('Choose a discipline')
-            .end()
-            .click();
-
-            $Disciplines.find('a').click(function (e) {
-                e.preventDefault();
-                $DisciplinesSelector.find('span').text($(this).text());
-
-                var hash = $(this).attr('href').replace('#', '');
-                loadWinnersOf(hash);
-         });
-    }
-
-    function loadWinnersOf (mid) {
-        $Winners.addClass('loading').html('Loading...');
-        $Winners.load('dispatch.php?source=freebase&type=winners&params=' + mid, null, function () {
-            $Winners
-                .removeClass('loading')
-                .find('a').click(function (e) {
-                    e.preventDefault();
-                    loadPersonalInfo($(this));
-                });
-
-            showMap();
-        });
-    }
-
-    function loadPersonalInfo ($link) {
-        var $Info = $link.after('<div class="infoblock loading">Loading...</div>').siblings('.infoblock');
-        var $Photos = $Info.after('<div class="photos" id="Lightbox' + Math.round(Math.random() * 10000) + '"></div>').siblings('.photos');
-        var slug = $link.attr('data-wiki');
-
-        loadPhotos($Photos, slug);
-
-        $Info.load('dispatch.php?source=dbpedia&type=bio&params=' + slug, null, function () {
-            $Info.removeClass('loading');
-        });
-    }
-
-    function loadPhotos ($Photos, slug) {
-        $Photos.load('dispatch.php?source=flickr&type=photos&params=' + slug);
-    }
-
-    loadOlympics();
-
     var markers = [];
-    function showMap () {
+
+    this.hideMap = function () {
+        $Map.hide();
+    }
+    this.showMap = function ($Winners) {
         $Map.show();
         $.each(markers, function (i, m) {
             m.setMap(null);
@@ -185,4 +54,146 @@ jQuery(document).ready(function ($) {
             }
         });
     }
+}).apply(map);
+
+(function () {
+    var _this = this;
+    var $OGSelector = $('#OGSelector');
+    var $Olympics = $('#Olympics');
+
+    var $EventsSelector = $('#EventsSelector');
+    var $Events = $('#Events');
+
+    var $DisciplinesSelector = $('#DisciplinesSelector');
+    var $Disciplines = $('#Disciplines');
+
+    var $Winners = $('#Winners');
+    this.removeStringFromElements = function (str, $els) {
+        $els.each(function (i, e) {
+            $(e).text($(e).text().replace(str, ''));
+        });
+    }
+
+    this.loadOlympics = function () {
+        $Olympics.load('dispatch.php?source=freebase&type=games', null, function () {
+            _this.olympicsLoaded();
+        });
+    }
+    this.olympicsLoaded = function () {
+        $OGSelector
+            .removeClass('loading')
+            .find('span')
+            .text('Choose Olympic Games')
+            .end()
+            .click();
+
+        $Olympics.find('a').click(function (e) {
+            e.preventDefault();
+
+            $OGSelector.find('span').text($(this).text()).attr('data-og_name', $(this).attr('data-og_name'));
+            var mid = $(this).attr('href').replace('#', '');;
+
+            _this.loadEventsFor(mid);
+        });
+    }
+
+    this.loadEventsFor = function (mid) {
+        $EventsSelector
+            .parent('.hide')
+            .removeClass('hide')
+            .end()
+            .addClass('loading')
+            .find('span').text('Loading...');
+
+        $Events.load('dispatch.php?source=freebase&type=events&params=' + mid, null, function () {
+            var currentName = new RegExp(' at (the)? ' + $OGSelector.find('span').attr('data-og_name') + '?');
+            _this.removeStringFromElements(currentName, $Events.find('li a'));
+            _this.eventsLoaded();
+        });
+    }
+    this.eventsLoaded = function () {
+        $EventsSelector
+            .removeClass('loading')
+            .find('span')
+            .text('Choose an event')
+            .end()
+            .click();
+
+        $Events.find('a').click(function (e) {
+            e.preventDefault();
+
+            $EventsSelector.find('span').text($(this).text());
+            var mid = $(this).attr('href').replace('#', '');
+
+            _this.loadDisciplinesFor(mid);
+        });
+    }
+
+    this.loadDisciplinesFor = function (mid) {
+        $DisciplinesSelector
+            .parent('.hide')
+            .removeClass('hide')
+            .end()
+            .addClass('loading')
+            .find('span').text('Loading...');
+
+        $Disciplines.load('dispatch.php?source=freebase&type=disciplines&params=' + mid, null, function () {
+            var currentName = new RegExp($EventsSelector.find('span').text() + ' at (the)? ' + $OGSelector.find('span').attr('data-og_name') + '?' + ' – ');
+            _this.removeStringFromElements(currentName, $Disciplines.find('li a'));
+            _this.disciplinesLoaded();
+        });
+    }
+
+    this.disciplinesLoaded = function () {
+        $DisciplinesSelector
+            .removeClass('loading')
+            .find('span')
+            .text('Choose a discipline')
+            .end()
+            .click();
+
+        $Disciplines.find('a').click(function (e) {
+            e.preventDefault();
+            $DisciplinesSelector.find('span').text($(this).text());
+
+            var hash = $(this).attr('href').replace('#', '');
+            _this.loadWinnersOf(hash);
+        });
+    }
+
+    this.loadWinnersOf = function (mid) {
+        $Winners.addClass('loading').html('Loading...');
+        $Winners.load('dispatch.php?source=freebase&type=winners&params=' + mid, null, function () {
+            $Winners
+                .removeClass('loading')
+                .find('a').click(function (e) {
+                    e.preventDefault();
+                    _this.loadPersonalInfo($(this));
+                });
+
+            map.showMap($Winners);
+        });
+    }
+
+    this.loadPersonalInfo = function ($link) {
+        var $Info = $link.after('<div class="infoblock loading">Loading...</div>').siblings('.infoblock');
+        var $Photos = $Info.after('<div class="photos" id="Lightbox' + Math.round(Math.random() * 10000) + '"></div>').siblings('.photos');
+        var slug = $link.attr('data-wiki');
+
+        _this.loadPhotos($Photos, slug);
+
+        $Info.load('dispatch.php?source=dbpedia&type=bio&params=' + slug, null, function () {
+            $Info.removeClass('loading');
+        });
+    }
+
+    this.loadPhotos = function ($Photos, slug) {
+        $Photos.load('dispatch.php?source=flickr&type=photos&params=' + slug);
+    }
+
+}).apply(rzzw);
+
+jQuery(document).ready(function ($) {
+    rzzw.loadOlympics();
+    map.hideMap();
 });
